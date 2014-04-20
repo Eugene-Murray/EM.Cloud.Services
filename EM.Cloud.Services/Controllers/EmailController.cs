2116738c569cc9de7830a9943a3c94f38384e51b
Eugene-Murray <eugene.murray@gmail.com>
2014-04-20 12:57:16 +0100

    add try catch

--- EM.Cloud.Services/Controllers/EmailController.cs
+++ EM.Cloud.Services/Controllers/EmailController.cs
@@ -77,46 +77,51 @@ namespace EM.Cloud.Services.Controllers
         // GET api/email
         public List<string>[] Get()
         {
-            string query = "SELECT * FROM Emails";
-
-            //Create a list to store the result
-            List<string>[] list = new List<string>[3];
-            list[0] = new List<string>();
-            list[1] = new List<string>();
-            list[2] = new List<string>();
-
-            //Open connection
-            if (this.OpenConnection() == true)
+            try
             {
-                //Create Command
-                MySqlCommand cmd = new MySqlCommand(query, connection);
-                //Create a data reader and Execute the command
-                MySqlDataReader dataReader = cmd.ExecuteReader();
 
-                //Read the data and store them in the list
-                while (dataReader.Read())
+                string query = "SELECT * FROM Emails";
+
+                //Create a list to store the result
+                List<string>[] list = new List<string>[3];
+                list[0] = new List<string>();
+                list[1] = new List<string>();
+                list[2] = new List<string>();
+
+                //Open connection
+                if (this.OpenConnection() == true)
                 {
-                    list[0].Add(dataReader["Id"] + "");
-                    list[1].Add(dataReader["WebsiteId"] + "");
-                    list[2].Add(dataReader["Email"] + "");
+                    //Create Command
+                    MySqlCommand cmd = new MySqlCommand(query, connection);
+                    //Create a data reader and Execute the command
+                    MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                    //Read the data and store them in the list
+                    while (dataReader.Read())
+                    {
+                        list[0].Add(dataReader["Id"] + "");
+                        list[1].Add(dataReader["WebsiteId"] + "");
+                        list[2].Add(dataReader["Email"] + "");
+                    }
+
+                    //close Data Reader
+                    dataReader.Close();
+
+                    //close Connection
+                    this.CloseConnection();
+
+                    //return list to be displayed
+                    return list;
+                }
+                else
+                {
+                    return list;
                 }
 
-                //close Data Reader
-                dataReader.Close();
-
-                //close Connection
-                this.CloseConnection();
-
-                //return list to be displayed
-                return list;
             }
-            else
-            {
-                return list;
+            catch(Exception ex){
+                throw ex;
             }
-            
-            
-            
             
           
         }
